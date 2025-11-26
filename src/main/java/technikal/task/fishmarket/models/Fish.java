d7e904df029854a5b9a1d26df9058a6a9c015944
Vladyslav Shupliakov <shupliakovvl@gmail.com>
2025-11-26 13:35:12 +0200

    feature/fishmarket-improvements: task solution

--- src/main/java/technikal/task/fishmarket/models/Fish.java
+++ src/main/java/technikal/task/fishmarket/models/Fish.java
@@ -2,53 +2,65 @@ package technikal.task.fishmarket.models;
 
 import java.util.Date;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "fish")
 public class Fish {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	private String name;
 	private double price;
 	private Date catchDate;
-	private String imageFileName;
-	
+	@OneToMany(mappedBy = "fish", cascade = CascadeType.ALL, orphanRemoval = true)
+	private java.util.List<FishImage> images = new java.util.ArrayList<>();
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public Date getCatchDate() {
 		return catchDate;
 	}
+
 	public void setCatchDate(Date catchDate) {
 		this.catchDate = catchDate;
 	}
-	public String getImageFileName() {
-		return imageFileName;
+
+	public java.util.List<FishImage> getImages() {
+		return images;
 	}
-	public void setImageFileName(String imageFileName) {
-		this.imageFileName = imageFileName;
+
+	public void setImages(java.util.List<FishImage> images) {
+		this.images = images;
 	}
 
 }
--- src/main/java/technikal/task/fishmarket/models/FishDto.java
+++ src/main/java/technikal/task/fishmarket/models/FishDto.java
@@ -6,31 +6,35 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 
 public class FishDto {
-	
 
 	@NotEmpty(message = "потрібна назва рибки")
 	private String name;
 	@Min(0)
 	private double price;
-	private MultipartFile imageFile;
-	
+	private java.util.List<MultipartFile> imageFiles;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	public MultipartFile getImageFile() {
-		return imageFile;
+
+	public java.util.List<MultipartFile> getImageFiles() {
+		return imageFiles;
 	}
-	public void setImageFile(MultipartFile imageFile) {
-		this.imageFile = imageFile;
+
+	public void setImageFiles(java.util.List<MultipartFile> imageFiles) {
+		this.imageFiles = imageFiles;
 	}
 
 }
